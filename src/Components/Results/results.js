ee676c679c0b5d4b4b330cf1e294aa0b79a94e0a
Max <riegelmax@outlook.com>
2022-07-07 16:57:52 +0200

    new build

---
+++ src/Components/Results/results.js
@@ -0,0 +1,41 @@
+import react from "react"
+import React, {useState, useEffect} from "react"
+import "./results.css"
+
+export function Button2 (){
+   const [clicked, setClicked] = useState(0)
+
+
+
+const ingredients = ["Gurken", "Anis", "Milch", "Nuss", "Sahne", "Hühnchen", "Karotten", "Suppe", "Smoothie"]
+const types = ["Kaffee", "Tee", "Drink", "Schnaps", "Shake", "Mix"]
+const adjectives = ["Lecker:", "Natürlich:", "Super gesund:", "Abenteuerlich:", "Tibetanischer Style:"]
+
+var ing1 = ingredients[Math.floor(Math.random()*ingredients.length)]
+var ing2 = ingredients[Math.floor(Math.random()*ingredients.length)]
+
+const adj = adjectives[Math.floor(Math.random()*adjectives.length)]
+const type = types[Math.floor(Math.random()*types.length)]
+
+//verhindern, dass zwei Zutaten gleich sind:
+do {ing2 = ingredients[Math.floor(Math.random()*ingredients.length)]
+}
+while(ing1 === ing2)
+
+const drink = adj + " " + ing1 + " - "+ ing2 + " " + type
+
+
+
+
+return <div className="Buttondiv2">
+    <button className="Button2" onClick={()=>setClicked(clicked+1)}>Namen generieren</button>
+        <p className="result">{drink}</p>
+        </div>
+   
+}
+export default Button2
+
+
+
+
+
